adb2593394e47195ee32f8edc81ede4dd3bf05e0
Arjun-Suresh <arjun.sureshn2000@gmail.com>
2017-09-12 18:51:56 -0500

    Programming assignment 1, Part 2
    Submitted the following function implementations:
    1. Appending 'Hello, ' to input name
    2. Check if string starts with consonant
    3. Check if string is a multiple of 4

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -36,15 +36,37 @@ end
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+	# YOUR CODE HERE
+	return "Hello, "+name
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+	# YOUR CODE HERE
+	if ['A', 'a', 'E', 'e', 'I', 'i', 'O', 'o', 'U', 'u'].include? s[0] or ( not ('a'..'z') === s[0] and not ('A'..'Z') === s[0])
+		return false
+	else
+		return true
+	end
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+	# YOUR CODE HERE
+	reversestring = s.reverse
+	k=0
+	value=0
+	for index in 0...reversestring.size
+		if not ['0','1'].include? reversestring[index]
+			return false
+		else
+			value = value+reversestring[index].to_i * 2**k
+			k+=1
+		end
+	end
+	if value % 4 == 0 and k>0
+		return true
+	else
+		return false
+	end
 end
 
 # Part 3
