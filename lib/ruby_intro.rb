4a5039b36be5d2b41f2d53ccbcb10bfcb653cfe5
Arjun-Suresh <arjun.sureshn2000@gmail.com>
2017-09-12 18:48:39 -0500

    Programming assignment 1, part 1
    Made changes to incorporate changes that were required for programming assignment 1 (Introduction to ruby) part 1.
    3 functions implemented are:
    1) Sum of array
    2) Sum of largest 2 values
    3) Check if sum of 2 numbers equal n

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,15 +3,34 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+	# YOUR CODE HERE
+	total=0
+	arr.each{|value| total += value}
+	return total
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+	# YOUR CODE HERE
+	total=0
+	if arr.length < 2
+		arr.each {|i| total+=i}
+	else
+		newarray = arr.sort {|x,y| y<=>x}
+		total+=newarray[0]+newarray[1]
+	end
+	return total
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+	# YOUR CODE HERE
+	for icounter in 0 ... arr.size-1
+		for jcounter in icounter+1 ... arr.size
+			if arr[icounter] + arr[jcounter] == n
+			return true
+			end
+		end
+	end
+	return false
 end
 
 # Part 2
